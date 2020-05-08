4f6385634ca8f06b8ed2d6bed81b79ecbdd92efa
Andrea <bomb-94@hotmail.it>
2020-05-08 14:36:04 +0200

    added markdown support

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import Layout from '../components/layout'
+import React from "react"  
+import { Link, graphql } from "gatsby"  
+import Img from "gatsby-image"  
+import Layout from "../components/layout"  
+import ReactMarkdown from "react-markdown"
+import "../styles/global.css"  
 
-const IndexPage = ({ data }) => (
+const IndexPage = ({ data }) => (  
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -14,8 +16,15 @@ const IndexPage = ({ data }) => (
           <h2>
             <Link to={`/${document.node.id}`}>{document.node.title}</Link>
           </h2>
-          <Img fixed={document.node.image.childImageSharp.fixed}/>
-          <p>{document.node.content}</p>
+          <Img fixed={document.node.image.childImageSharp.fixed} />
+          <ReactMarkdown  
+            source={document.node.content.substring(0, 500).concat("...")}
+            transformImageUri={uri => uri.startsWith('http') ? uri : `${process.env.IMAGE_BASE_URL}${uri}`}
+            className="indexArticle"
+            escapeHtml={false}
+          />
+
+          <Link to={`/${document.node.id}`}>Read more</Link>
         </li>
       ))}
     </ul>
--- src/templates/author.js
+++ src/templates/author.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/layout'
+import React from "react"  
+import { Link, graphql } from "gatsby"  
+import Layout from "../components/layout"  
+import ReactMarkdown from "react-markdown"
 
-const UserTemplate = ({ data }) => (
-    <Layout>
-      <h1>{data.strapiUser.username}</h1>
-      <ul>
-        {data.strapiUser.articles.map(article => (
-          <li key={article.id}>
-            <h2>
-              <Link to={`/Article_${article.id}`}>{article.title}</Link>
-            </h2>
-            <p>{article.content}</p>
-          </li>
-        ))}
-      </ul>
-    </Layout>
-  )
-  
+const UserTemplate = ({ data }) => (  
+  <Layout>
+    <h1>{data.strapiUser.username}</h1>
+    <ul>
+      {data.strapiUser.articles.map(article => (
+        <li key={article.id}>
+          <h2>
+            <Link to={`/Article_${article.id}`}>{article.title}</Link>
+          </h2>
+          <ReactMarkdown  
+            source={article.content.substring(0, 500).concat("...")}
+            transformImageUri={uri => uri.startsWith('http') ? uri : 
+            `${process.env.IMAGE_BASE_URL}${uri}` }
+            className="indexArticle" 
+            escapeHtml={false}
+          />
+
+          <Link to={`/Article_${article.id}`}>Read more</Link>  
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+
 export default UserTemplate
 
-export const query = graphql`
+export const query = graphql`  
   query UserTemplate($id: String!) {
     strapiUser(id: { eq: $id }) {
       id
@@ -32,4 +42,4 @@ export const query = graphql`
       }
     }
   }
-` 
+`
